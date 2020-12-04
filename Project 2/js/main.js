4c23c32e428c107c1002472c108615a727dfcc9d
Hadley <56000913+hadnels9@users.noreply.github.com>
2020-12-04 12:41:01 -0800

    EDIT 12/4

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -1,6 +1,5 @@
 var mybutton = document.getElementById("myBtn");
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
